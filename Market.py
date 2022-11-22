9a9ca7d8c833345d7150fdf60d847331ca668ebb
João Henrique dos Santos Macedo <72304764+joa738@users.noreply.github.com>
2022-11-22 20:49:34 -0300

    Firstone
    O meu primeiro projeto

---
+++ Market.py
@@ -0,0 +1,9 @@
+from ast import Div
+from tkinter import Menu
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('juliene')
+def home_page():
+    return render_template ('home.html')
+
+
