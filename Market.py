d7396814b76e9293a04ff427a96735e6c5d967ad
João Henrique dos Santos Macedo <72304764+joa738@users.noreply.github.com>
2022-11-24 12:02:11 -0300

    Delete Market.py

--- Market.py
+++
@@ -1,9 +0,0 @@
-from ast import Div
-from tkinter import Menu
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/home')
-def home_page():
-    return render_template ('home.html')
-
-
